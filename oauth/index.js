c60e93f485e9d59b9ff841323888a53107d3f822
seniordev0312 <devsenior0312@gmail.com>
2023-08-10 14:09:03 +0100

    oauth

--- oauth/index.js
+++ oauth/index.js
@@ -1,12 +1,59 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = require('url');
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+// parameter for authentication
+const APP_ID = '7KGNJY45YD9V2';
+// const CLIENT_REDIRECT_URL = '730dbcbc-c4b1-6a9b-1b4e-ffba0fad26f0';
+const MERCHANT_ID = 'GCMHPZQAE3A81';
+const AUTHORIZATION_CODE = '';
+const APP_SECRET = '730dbcbc-c4b1-6a9b-1b4e-ffba0fad26f0';
+const REDIRECT_URI = 'http://localhost:3000/redirect-url';
+
+const server = http.createServer(async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname, query } = parsedUrl;
+  // console.log('pathname')
+  if (pathname === '/redirect-url') {
+    const authorizationCode = query.code;
+    console.log('Authorization Code:', authorizationCode);
+
+    axios
+      .get(
+        `https://sandbox.dev.clover.com/oauth/token?client_id=${APP_ID}&client_secret=${APP_SECRET}&code=${authorizationCode}`,
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+
+    res.writeHead(302, { Location: '/success' });
+    res.end();
+  } else {
+    // Handle other routes or requests
+    res.writeHead(404);
+    res.end();
+  }
 });
 
+async function getAuthorizationCode() {
+  axios
+    .get(
+      `https://sandbox.dev.clover.com/oauth/authorize?client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}`,
+    )
+    .then((response) => {
+      const htmlUrl = response.request.res.responseUrl;
+
+      console.log('HTML URL:', htmlUrl);
+    });
+}
+
 server.listen(port, () => {
+  getAuthorizationCode();
+
   console.log(`Server running on http://localhost:${port}/`);
 });
